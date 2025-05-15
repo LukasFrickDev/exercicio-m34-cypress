7c383e3d18fc88a330bd6148b94bddc9a16c8820
Lukas Frick <fricklukas96@gmail.com>
2025-05-15 15:02:18 -0300

    correcao-exercicio

--- cypress/e2e/E_M34/incluir-alt-rem.cy.js
+++ cypress/e2e/E_M34/incluir-alt-rem.cy.js
@@ -10,8 +10,14 @@ describe('Teste para Agenda de contato', () => {
         cy.get('input[type="email"]').type('fricklukas96@gmail.com')
         cy.get('input[type="tel"]').type('958244081')
         cy.get('.adicionar').first().click()
+
         cy.get('input').should('have.length', 3)
+        cy.contains('Lukas Frick').should('exist')
+        cy.contains('fricklukas96@gmail.com').should('exist')
+        cy.contains('958244081').should('exist')
+
         cy.screenshot('incluindo-contato')
+
     })
 
     it('Deve alterar os dados do contato adicionado', () => {
@@ -22,19 +28,30 @@ describe('Teste para Agenda de contato', () => {
         cy.get('input[type="email"]').type('fricklukas96novo@gmail.com')
         cy.get('input[type="tel"]').clear()
         cy.get('input[type="tel"]').type('11958244081')
-        cy.screenshot('incluindo-contato-alteracao')
         cy.get('.alterar').click()
+
+        cy.wait(1000);      
+        cy.contains('Lukas C. Frick').should('exist')
+        cy.contains('fricklukas96novo@gmail.com').should('exist')
+        cy.contains('11958244081').should('exist')
+
+        cy.screenshot('incluindo-contato-alteracao')
     })
 
     it('Deve cancelar a alteraçao dos dados do contato adicionado', () => {
         cy.get(':nth-child(4) > .sc-gueYoa > .edit').click()
         cy.get('input[type="text"]').clear()
-        cy.get('input[type="text"]').type('Lukas C. Frick')
+        cy.get('input[type="text"]').type('Lukas Frick')
         cy.get('.cancelar').click()
+
+        cy.wait(1000);      
+        cy.contains('Lukas C. Frick').should('exist')
     })
 
     it('Deve deletar os dados do contato adicionado', () => {
         cy.get(':nth-child(4) > .sc-gueYoa > .delete').click()
+        cy.wait(1000);
+        cy.contains('Lukas C. Frick').should('not.exist')
         cy.screenshot('removendo-contato')
 
     })
